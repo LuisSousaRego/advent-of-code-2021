9f54085e7f9903fb56dbc2150e38f3324f07f16d
Luís Sousa Rêgo <luis.sousa.rego@gmail.com>
2021-12-01 19:09:50 +0000

    day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn main() {
+fn part1() {
     let filename = "puzzleInputs/day1.txt";
     let file = File::open(filename).unwrap();
     let reader = BufReader::new(file);
 
     let mut counter = 0;
     let mut prev_depth = i32::MAX;
-    for (_, line) in reader.lines().enumerate() {
+    for line in reader.lines() {
         let line = line.unwrap();
         let depth: i32 = line.parse().unwrap();
 
@@ -17,5 +17,35 @@ fn main() {
         }
         prev_depth = depth
     }
-    println!("{}", counter);
+    println!("part 1: {}", counter);
+}
+
+fn part2() {
+    let filename = "puzzleInputs/day1.txt";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut depth_arr: [i32; 2000] = [0; 2000];
+    for (i, line) in reader.lines().enumerate() {
+        let line = line.unwrap();
+        let depth: i32 = line.parse().unwrap();
+
+        depth_arr[i] = depth
+    }
+
+    let mut counter = 0;
+
+    for i in 0..(2000 - 3) {
+        let first_three = depth_arr[i] + depth_arr[i + 1] + depth_arr[i + 2];
+        let second_three = depth_arr[i + 1] + depth_arr[i + 2] + depth_arr[i + 3];
+
+        if second_three > first_three {
+            counter += 1;
+        }
+    }
+    println!("part 2: {}", counter);
+}
+
+fn main() {
+    part1();
+    part2();
 }
